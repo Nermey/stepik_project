5969c5cba54e600e8d0e5e0a45d86c7923496a1e
Nermey <elik19898@gmail.com>
2024-12-06 22:14:50 +0300

    refactor courses by param

--- stepik api test/get courses by params.py
+++ stepik api test/get courses by params.py
@@ -7,37 +7,38 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
-
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
-# Открываем страницу
-url = "https://stepik.org/catalog/search?free=true&q=Python"
-driver.get(url)
 
-# Ждем появления элемента с заголовком курса
-try:
-    WebDriverWait(driver, 40).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "course-card__title"))
-    )
-    html = driver.page_source
-finally:
-    # Закрываем браузер
-    driver.quit()
+def get_course(query: str) -> tuple:
+    # Открываем страницу
+    url = f"https://stepik.org/catalog/search?free=true&q={query}"
+    driver.get(url)
 
-# Парсинг с BeautifulSoup
-soup = BeautifulSoup(html, "lxml")
+    # Ждем появления элемента с заголовком курса
+    try:
+        WebDriverWait(driver, 40).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "course-card__title"))
+        )
+        html = driver.page_source
+    finally:
+        driver.quit()
 
-# Извлечение курсов
-courses = soup.find_all("a", class_="course-card__title")  # Класс заголовка курса
+    # Парсинг с BeautifulSoup
+    soup = BeautifulSoup(html, "lxml")
 
-if courses:
-    for course in courses:
-        link = course.get("href")
-        name = course.text.strip()
-        print(link, name)
-else:
-    print("Курсы не найдены.")
+    # Извлечение курсов
+    courses = soup.find("a", class_="course-card__title")  # Класс заголовка курса
+
+    return courses.get("href"), courses.text.strip() if courses else None
+
+
+print(get_course("sql"))
+print(get_course("python"))
+print(get_course("git"))
+print(get_course("fast api"))
+print(get_course("ООП"))
